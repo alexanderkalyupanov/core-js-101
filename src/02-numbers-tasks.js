75b1cbd558b2e5bf18a5839ba906a9f9f9db2d7a
alexanderkalyupanov <138789735+alexanderkalyupanov@users.noreply.github.com>
2024-09-12 17:33:19 +0300

    feat: start arrays-tasks

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -207,7 +207,7 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(n) {
+function isPrime() {
 
 }
 
@@ -226,8 +226,11 @@ function isPrime(n) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  if (Number.isFinite(value) || Number(value) === true) {
+    return value;
+  }
+  return def;
 }
 
 module.exports = {
